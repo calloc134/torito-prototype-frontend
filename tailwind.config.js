ebeef3937c1d4a8ba14c9599ffadfae67efeb025
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-06-15 22:54:17 +0900

    フォントの適用

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["Zen Kaku Gothic New", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
